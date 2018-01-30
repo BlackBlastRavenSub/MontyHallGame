29b593b517d8df40009fd5d4727b68b5c8f655f4
KOGUMA <smallbear0608@gmail.com>
2018-01-30 21:19:09 +0900

    エラーあり

--- app/src/main/java/com/game/blastraven/b/montyhallgame/TitleActivity.java
+++ app/src/main/java/com/game/blastraven/b/montyhallgame/TitleActivity.java
@@ -2,6 +2,9 @@ package com.game.blastraven.b.montyhallgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        /*final Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.d("debug", "button1, Perform action on click");
+                System.out.println("startButtonClicked!");
+            }
+        });*/
     }
 }
