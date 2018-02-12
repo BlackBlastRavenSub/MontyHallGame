0a7268145d94ea9e51785ea7042a69f73cdd6489
KOGUMA <smallbear0608@gmail.com>
2018-02-12 23:04:02 +0900

    LittleProgress...

--- app/src/main/java/com/game/blastraven/b/montyhallgame/TitleActivity.java
+++ app/src/main/java/com/game/blastraven/b/montyhallgame/TitleActivity.java
@@ -1,6 +1,8 @@
 package com.game.blastraven.b.montyhallgame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -35,10 +37,26 @@ public class TitleActivity extends AppCompatActivity {
     public void reading(View view) {
         Toast.makeText(this, "OK", Toast.LENGTH_LONG).show();
     }
+
+
     //Android端末の戻るボタン
     public boolean onKeyDown(int keyCode, KeyEvent event){
         if(keyCode==KeyEvent.KEYCODE_BACK){
-            this.moveTaskToBack(true);//アプリ全体の中断(できれば完全に終了させたい・・・)
+            new AlertDialog.Builder(TitleActivity.this)
+                    .setTitle("アプリを閉じようとしています")
+                    .setMessage("終了しますか？")
+                    .setIcon(R.drawable.exit)
+                    .setPositiveButton(
+                            "YES",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    moveTaskToBack(true);//アプリ全体の中断(できれば完全に終了させたい・・・)
+                                }
+                            }
+                    )
+                    .setNegativeButton("No",null)
+                    .show();
             return false;
         }
         return true;
